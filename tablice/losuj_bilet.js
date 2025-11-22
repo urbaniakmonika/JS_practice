982404f8736a08bca8dc779de61843179ef7e56f
urbaniakmonika <monika.urbaniak96@gmail.com>
2025-11-22 21:24:46 +0100

    małe zadanie

---
+++ tablice/losuj_bilet.js
@@ -0,0 +1,7 @@
+console.log('hi');
+
+let bilety = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110];
+
+let losowyIndex = Math.floor(Math.random()*bilety.length);
+let losowyBilet = bilety[losowyIndex];
+console.log(losowyBilet) 
